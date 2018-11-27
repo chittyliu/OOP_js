670f6a409bc8fab1223a472b098b5cba8c7942f4
Chitty Liu <d2921118@gmail.com>
2018-11-27 17:54:50 +0000

    Object Literals

---
+++ index.js
@@ -0,0 +1,14 @@
+const circle = {
+  //   property holds value
+  radious: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  //   functuion define logic
+  draw: function() {
+    console.log("draw");
+  }
+};
+
+circle.draw();
