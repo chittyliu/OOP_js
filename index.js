bf4373bcb96ffaa859ed9de970703925bee86e5c
Chitty Liu <d2921118@gmail.com>
2018-11-27 18:00:30 +0000

    Factory Function

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const circle = {
-  //   property holds value
-  radious: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  //   functuion define logic
-  draw: function() {
-    console.log("draw");
-  }
-};
+// Factory Function
+function createCircle(radious) {
+  return {
+    radious,
+    draw: function() {
+      console.log("draw");
+    }
+  };
+}
+const circle = createCircle(1);
+
+// const circle = {
+//   //   property holds value
+//   radious: 1,
+//   location: {
+//     x: 1,
+//     y: 1
+//   },
+//   //   functuion define logic
+//   draw: function() {
+//     console.log("draw");
+//   }
+// };
 
 circle.draw();
