ebb1aa5dfce9c8be759880fa4bda65864a7a49dd
Chitty Liu <d2921118@gmail.com>
2018-12-04 15:28:39 +0000

    Abstraction & Private Properties and Methods

--- index.js
+++ index.js
@@ -12,23 +12,37 @@
 // Constructor Function
 function Circle(radious) {
   this.radious = radious;
+
+  let defaultLocation = { x: 0, y: 0 };
+
+  let computeoptimumLocation = function(factor) {
+    // ...
+  };
+
   this.draw = function() {
+    computeoptimumLocation(0.1);
+    // defaultLocation();
+    // this.radious;
+
     console.log("draw");
   };
 }
 
 const circle = new Circle(10);
+circle.radious;
+circle.draw();
 
-for (let key in circle) {
-  if (typeof circle[key] !== "function")
-    // bracket notation
-    console.log(key, circle[key]);
-}
+// for (let key in circle) {
+//   if (typeof circle[key] !== "function")
+//     // bracket notation
+//     console.log(key, circle[key]);
+// }
 
-const keys = Object.keys(circle);
-console.log(keys);
+// const keys = Object.keys(circle);
+// console.log(keys);
 
-if ("radious" in circle) console.log("Circle has a radious.");
+// if ("radious" in circle) console.log("Circle has a radious.");
+
 // const circle = {
 //   //   property holds value
 //   radious: 1,
@@ -41,5 +55,3 @@ if ("radious" in circle) console.log("Circle has a radious.");
 //     console.log("draw");
 //   }
 // };
-
-// circle.draw();
