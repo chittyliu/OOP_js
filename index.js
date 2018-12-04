d9499f4e1f50b569589ff25c84bf1d620e3a61ef
Chitty Liu <d2921118@gmail.com>
2018-12-04 16:04:41 +0000

    Stopwatch exercise

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+// Stopwatch Exercise
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
+
+  this.start = function() {
+    if (running) throw new Error("Stopwatch has already started.");
+    running = true;
+
+    startTime = new Date();
+  };
+
+  this.stop = function() {
+    if (!running) throw new Error("Stopwatch has not started yet.");
+    running = false;
+
+    endTime = new Date();
+    duration = (endTime.getTime() - startTime.getTime()) / 1000;
+  };
+
+  this.reset = function() {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+
+  Object.defineProperty(this, "duration", {
+    get: function() {
+      return duration;
+    }
+  });
+}
+
 // Factory Function
 // function createCircle(radious) {
 //   return {
@@ -10,33 +46,33 @@
 // const circle = createCircle(1);
 
 // Constructor Function
-function Circle(radious) {
-  this.radious = radious;
+// function Circle(radious) {
+//   this.radious = radious;
 
-  let defaultLocation = { x: 0, y: 0 };
+//   let defaultLocation = { x: 0, y: 0 };
 
-  this.getDefaultLcation = function() {
-    return defaultLocation;
-  };
+//   this.getDefaultLcation = function() {
+//     return defaultLocation;
+//   };
 
-  this.draw = function() {
-    console.log("draw");
-  };
+//   this.draw = function() {
+//     console.log("draw");
+//   };
 
-  Object.defineProperty(this, "defaultLocation", {
-    get: function() {
-      return defaultLocation;
-    },
-    set: function(value) {
-      if (!value.x || !value.y) throw new Error("Invalid location.");
+//   Object.defineProperty(this, "defaultLocation", {
+//     get: function() {
+//       return defaultLocation;
+//     },
+//     set: function(value) {
+//       if (!value.x || !value.y) throw new Error("Invalid location.");
 
-      defaultLocation = value;
-    }
-  });
-}
+//       defaultLocation = value;
+//     }
+//   });
+// }
 
-const circle = new Circle(10);
-circle.draw();
+// const circle = new Circle(10);
+// circle.draw();
 // circle.defaultLocation = 1;
 
 // for (let key in circle) {
